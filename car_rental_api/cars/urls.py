e805d30fe72ffbf22e735d841b551e2c17aeb6e0
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 12:00:47 -0300

    refactor: change cars from ViewSets to @api_view

--- car_rental_api/cars/urls.py
+++ car_rental_api/cars/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from .views import car_list, car_create, car_detail, car_update, car_delete
 
 urlpatterns = [
-    path('carros/', views.CarroListCreate.as_view()),
-    path('carros/<int:pk>/', views.CarroRetrieveUpdateDestroy.as_view()),
+    path('cars/', car_list, name='car-list'),
+    path('cars/create/', car_create, name='car-create'),
+    path('cars/<int:pk>/', car_detail, name='car-detail'),
+    path('cars/<int:pk>/update/', car_update, name='car-update'),
+    path('cars/<int:pk>/delete/', car_delete, name='car-delete'),
 ]
--- car_rental_api/cars/views.py
+++ car_rental_api/cars/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Car
 from .serializers import CarSerializer
 
 # Create your views here.
-class CarroListCreate(generics.ListCreateAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
+@api_view(['GET'])
+def car_list(request):
+    """List all cars."""
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
+    return Response(serializer.data)
 
-class CarroRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
+@api_view(['POST'])
+def car_create(request):
+    """Create a new car."""
+    serializer = CarSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def car_detail(request, pk):
+    """Retrieve a car by ID."""
+    car = get_object_or_404(Car, pk=pk)
+    serializer = CarSerializer(car)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def car_update(request, pk):
+    """Update a car."""
+    car = get_object_or_404(Car, pk=pk)
+    serializer = CarSerializer(car, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def car_delete(request, pk):
+    """Delete a car."""
+    car = get_object_or_404(Car, pk=pk)
+    car.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
