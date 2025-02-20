4e98efebfcf4aeeff55c3a23c71048bb3d982be2
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 19:31:43 -0300

    chore: remove unused imports from RentalSerializer

--- car_rental_api/rental/serializers.py
+++ car_rental_api/rental/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from .models import Rental
-from car_renters.serializers import CarRenterSerializer
-from cars.serializers import CarSerializer
 
 class RentalSerializer(serializers.ModelSerializer):
     class Meta:
