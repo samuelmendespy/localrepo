6d0aa091164610ae5c1946bba6e036dab270e213
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 10:57:22 -0300

    feat: add cars views

--- car_rental_api/cars/views.py
+++ car_rental_api/cars/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Car
+from .serializers import CarSerializer
 
 # Create your views here.
+class CarroListCreate(generics.ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class CarroRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
