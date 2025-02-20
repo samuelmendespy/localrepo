57b151bef26fd98e0d0f083ace0645410ab6f20d
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 11:43:07 -0300

    refactor: change car renters model property to Enum

--- car_rental_api/car_renters/models.py
+++ car_rental_api/car_renters/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class RendaChoices(models.IntegerChoices):
+    BAD = 1, "Bad"
+    GOOD = 2, "Good"
+    PERFECT = 3, "Perfect"
+
 class CarRenter(models.Model):
     name = models.CharField(max_length=100)
-    renda = models.DecimalField(max_digits=10, decimal_places=2)
+    renda = models.IntegerField(choices=RendaChoices.choices, default=RendaChoices.BAD)
 
     def __str__(self):
         return self.name
