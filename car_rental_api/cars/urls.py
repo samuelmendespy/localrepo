d84fc011b60491a398e153f8e4a83c5f9b5e0f92
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 11:41:04 -0300

    chore: remove rent url rom cars urls

--- car_rental_api/cars/urls.py
+++ car_rental_api/cars/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('carros/', views.CarroListCreate.as_view()),
     path('carros/<int:pk>/', views.CarroRetrieveUpdateDestroy.as_view()),
-    path('carros/<int:carro_id>/atribuir/<int:locatario_id>/', views.atribuir_locatario),
 ]
