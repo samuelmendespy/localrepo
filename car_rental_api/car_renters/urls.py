bf9ed880075b3b78733002bc6ae27b8ca9af97dd
samuelmendespy <samuelmendespy@gmail.com>
2025-02-20 14:27:36 -0300

    refactor: revert car renters api to ModelViewSet (#3)

--- car_rental_api/car_renters/urls.py
+++ car_rental_api/car_renters/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from .views import renter_list, renter_create, renter_detail, renter_update, renter_delete
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CarRenterViewSet
+
+router = DefaultRouter()
+router.register(r'renters', CarRenterViewSet)
 
 urlpatterns = [
-    path('renters/', renter_list, name='renter-list'),
-    path('renters/create/', renter_create, name='renter-create'),
-    path('renters/<int:pk>/', renter_detail, name='renter-detail'),
-    path('renters/<int:pk>/update/', renter_update, name='renter-update'),
-    path('renters/<int:pk>/delete/', renter_delete, name='renter-delete'),
+    path('', include(router.urls)),
 ]
--- car_rental_api/car_renters/views.py
+++ car_rental_api/car_renters/views.py
@@ -1,47 +1,9 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 from .models import CarRenter
 from .serializers import CarRenterSerializer
 
 # Create your views here.
-@api_view(['GET'])
-def renter_list(request):
-    """List all car renters."""
-    renters = CarRenter.objects.all()
-    serializer = CarRenterSerializer(renters, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def renter_create(request):
-    """Create a new car renter."""
-    serializer = CarRenterSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def renter_detail(request, pk):
-    """Retrieve a car renter by ID."""
-    renter = get_object_or_404(CarRenter, pk=pk)
-    serializer = CarRenterSerializer(renter)
-    return Response(serializer.data)
-
-@api_view(['PUT'])
-def renter_update(request, pk):
-    """Update a car renter."""
-    renter = get_object_or_404(CarRenter, pk=pk)
-    serializer = CarRenterSerializer(renter, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-def renter_delete(request, pk):
-    """Delete a car renter."""
-    renter = get_object_or_404(CarRenter, pk=pk)
-    renter.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+class CarRenterViewSet(viewsets.ModelViewSet):
+    """CarRenter CRUD with list, create, retrieve and update"""
+    queryset = CarRenter.objects.all()
+    serializer_class = CarRenterSerializer
