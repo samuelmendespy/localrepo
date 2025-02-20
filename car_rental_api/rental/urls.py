a29b6d17562e4aa6727896150b24e557c5840cbf
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-20 17:45:57 -0300

    chore: remove / from rental endpoints

--- car_rental_api/rental/urls.py
+++ car_rental_api/rental/urls.py
@@ -4,8 +4,8 @@ from .views import rental_list, rental_create, rental_detail, rental_delete
 
 urlpatterns = [
     path('', rental_list, name='rental-list'),
-    path('/create/', rental_create, name='rental-create'),
-    path('/<int:pk>/', rental_detail, name='rental-detail'),
-    path('/<int:pk>/delete/', rental_delete, name='rental-delete'),
+    path('create/', rental_create, name='rental-create'),
+    path('<int:pk>/', rental_detail, name='rental-detail'),
+    path('<int:pk>/delete/', rental_delete, name='rental-delete'),
     
 ]
