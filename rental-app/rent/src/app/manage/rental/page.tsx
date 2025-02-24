01b717ec3f43e4550c04c65ee876ad7f745ae4b0
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-23 21:02:46 -0300

    refactor: update rental page

--- rental-app/rent/src/app/manage/rental/page.tsx
+++ rental-app/rent/src/app/manage/rental/page.tsx
@@ -1,13 +1,41 @@
 "use client";
+import { useState } from "react";
 
-const rentals = [
-  { id: 1, name: "John Doe", salary: 250 },
-  { id: 2, name: "Jane Doe", salary: 250 },
-  { id: 3, name: "Smith Karl", salary: 220 },
-  { id: 4, name: "Mark Luke", salary: 240 },
-  { id: 5, name: "Bob Nova", salary: 300 },
+const sampleCar: Car = {
+  id: 1,
+  minimun_salary: 300,
+  name: "Santa Fé",
+};
+
+const sampleUser: CarRenter = {
+  id: 1,
+  name: "John Doe",
+  salary: 300,
+};
+
+const rentals: Rental[] = [
+  { id: 1, car: sampleCar, renter: sampleUser, start_date: "", end_date: "" },
 ];
 
 export default function RentalPage() {
+  const [sortedRentals, setSortedRentals] = useState<Rental[]>(rentals);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: "asc" });
+
+  const sortData = (key) => {
+    let direction = "asc";
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
+      direction = "desc";
+    }
+
+    const sorted = [...sortedRentals].sort((a, b) => {
+      if (a[key] < b[key]) return direction === "asc" ? -1 : 1;
+      if (a[key] > b[key]) return direction === "asc" ? 1 : -1;
+      return 0;
+    });
+
+    setSortedRentals(sorted);
+    setSortConfig({ key, direction });
+  };
+
   return <div className="container">Rentals here</div>;
 }
