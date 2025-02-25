22375a3802ecace2caa20391d328715c1f2ef904
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-24 21:13:37 -0300

    refactor: replace header with button to hide sidebar

--- rent/src/app/manage/layout.tsx
+++ rent/src/app/manage/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 import "./../globals.css";
 import SidebarLeft from "@/components/SidebarLeft";
+import { Icon } from "@mdi/react";
+import { mdiMenuOpen } from "@mdi/js";
 import { useState } from "react";
 
 export default function ManageLayout({
@@ -18,7 +20,12 @@ export default function ManageLayout({
       />
       <div className="flex-1 flex flex-col">
         <header>
-          <h1>Next App</h1>
+          <button
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            className={`p-2 rounded-md bg-dark hover:bg-gray-300 ${isCollapsed ? "block" : "hidden"}`}
+          >
+            <Icon path={mdiMenuOpen} size={1} />
+          </button>
         </header>
         <main className="flex-1 p-6 overflow-auto">{children}</main>
       </div>
