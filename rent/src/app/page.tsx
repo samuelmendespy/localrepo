12c3a9b20c61444f8dd481ab48f58a8817dd8275
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-25 16:13:09 -0300

    chore: update app page

--- rent/src/app/page.tsx
+++ rent/src/app/page.tsx
@@ -2,12 +2,11 @@ import Link from "next/link";
 import Icon from "@mdi/react";
 import {
   mdiAccountGroup,
-  mdiCar,
+  mdiTruck,
   mdiCalendar,
   mdiFileDocumentMultipleOutline,
   mdiBookOpenOutline,
   mdiWeb,
-  mdiTruck,
 } from "@mdi/js";
 import Image from "next/image";
 
@@ -33,7 +32,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col">
+        <div className="flex flex-col mb-12">
           <Image
             className="white:invert"
             src="/logo.svg"
@@ -46,7 +45,7 @@ export default function Home() {
             Assigner - A simple solution to allocate all drivers
           </p>
         </div>
-        <ol className="flex flex-wrap justify-center sm:justify-start gap-4 text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="flex flex-wrap justify-center sm:justify-start gap-4 text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] mb-12 pb-12">
           {items.map((item, index) => (
             <li key={index}>
               <Link href={item.href}>
