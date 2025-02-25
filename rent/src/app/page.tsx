a2bc138468a2921e9c580a335686ec4400e1aa06
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-24 21:14:03 -0300

    chore: page variante

--- rent/src/app/page.tsx
+++ rent/src/app/page.tsx
@@ -7,24 +7,25 @@ import {
   mdiFileDocumentMultipleOutline,
   mdiBookOpenOutline,
   mdiWeb,
+  mdiTruck,
 } from "@mdi/js";
 import Image from "next/image";
 
 export default function Home() {
   const items = [
     {
-      icon: <Icon path={mdiCar} size={3} />,
-      label: "Manage Cars",
+      icon: <Icon path={mdiTruck} size={3} />,
+      label: "Manage Trucks",
       href: "/manage/car",
     },
     {
       icon: <Icon path={mdiAccountGroup} size={3} />,
-      label: "Manage Renters",
+      label: "Manage Drivers",
       href: "/manage/renter",
     },
     {
       icon: <Icon path={mdiCalendar} size={3} />,
-      label: "Manage Rents",
+      label: "Assignments",
       href: "/manage/rental",
     },
   ];
@@ -32,14 +33,19 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/flam.svg"
-          alt="FLAM title"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="flex flex-col">
+          <Image
+            className="white:invert"
+            src="/logo.svg"
+            alt="A title"
+            width={40}
+            height={40}
+            priority
+          />
+          <p className="italic">
+            Assigner - A simple solution to allocate all drivers
+          </p>
+        </div>
         <ol className="flex flex-wrap justify-center sm:justify-start gap-4 text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           {items.map((item, index) => (
             <li key={index}>
@@ -52,40 +58,6 @@ export default function Home() {
             </li>
           ))}
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/logo.svg"
-              alt="FLAM logomark"
-              width={20}
-              height={20}
-            />
-            Menu Fun1
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Menu Fun2
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Menu Fun3
-          </a>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
