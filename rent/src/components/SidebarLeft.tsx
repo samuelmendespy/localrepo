7d733d8ee03d2c0cf2e4ef869b9ab2d0e0e4ac92
Samuel Mendes <samuelmendespy@gmail.com>
2025-02-25 13:35:03 -0300

    style: update sidebar style

--- rent/src/components/SidebarLeft.tsx
+++ rent/src/components/SidebarLeft.tsx
@@ -47,18 +47,17 @@ const SidebarLeft: React.FC<SidebarProps> = ({
 }) => {
   return (
     <div
-      className={`bg-gray-800 text-white h-screen transition-all duration-300 p-4 pl-2 flex flex-col justify-between ${isCollapsed ? "hidden" : "w-64"}`}
+      className={`bg-gray-800 text-white h-screen transition-all duration-300 p-4 pl-0 pr-0 flex flex-col justify-between ${isCollapsed ? "hidden" : "w-64"}`}
     >
       <div>
         <button
           onClick={toggleSidebar}
-          className={`absolute top-1 left-1 z-50 p-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none`}
+          className="absolute top-1 left-1 z-50 p-2 bg-gray-800 text-white rounded-full shadow-lg focus:outline-none"
         >
           <Icon path={mdiMenuClose} size={1} />
         </button>
         <div className="mb-4">
-          <p className={``}></p>
-          <div className={``}>
+          <div>
             <div className="flex justify-center mb-4">
               <Image
                 className="dark:invert"
@@ -71,7 +70,7 @@ const SidebarLeft: React.FC<SidebarProps> = ({
             </div>
             <div className="flex flex-col items-center">
               <div className="flex items-center space-x-2">
-                <span>DEMO ACCOUNT</span>
+                <span className="whitespace-nowrap">DEMO ACCOUNT</span>
                 <button
                   onClick={handleClick}
                   className="flex items-center text-white rounded-lg shadow-md hover:bg-blue-600"
@@ -85,15 +84,15 @@ const SidebarLeft: React.FC<SidebarProps> = ({
             </div>
           </div>
         </div>
-        <hr className={`border-t border-gray-300`} />
-        <div className={``}></div>
+        <hr className="border-t border-gray-300 mb-4" />
+        {/* Sidebar content */}
         <div className="flex flex-col gap-4">
-          <div className={`flex flex-col gap-4`}>
+          <div className="flex flex-col">
             {sidebartItems.map((item, index) => (
-              <div key={index} className="link-group flex flex-col gap-4">
+              <div key={index} className="link-group flex flex-col">
                 <Link href={item.href}>
-                  <div className="flex flex-row items-center w-40 rounded-2xl shadow-lg p-2">
-                    <div className="mb-1 mr-2">{item.icon}</div>
+                  <div className="flex flex-row items-center shadow-lg p-2 pl-4">
+                    <div className="mr-2">{item.icon}</div>
                     <span>{item.label}</span>
                   </div>
                 </Link>
@@ -102,9 +101,12 @@ const SidebarLeft: React.FC<SidebarProps> = ({
           </div>
         </div>
       </div>
-      <div className="mb-4">
-        <hr className={`border-t border-gray-300`} />
-        <p className={``}>Assigner v0.0.1</p>
+      <div className="pt-20">
+        <hr className="border-t border-gray-300" />
+      </div>
+
+      <div className="mb-2 ml-2">
+        <p className="items-center">Assigner v0.0.1</p>
       </div>
     </div>
   );
