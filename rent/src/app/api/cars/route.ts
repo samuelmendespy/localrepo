4322781e157264a84ad0d4dde8d3afdd627495ec
Samuel Mendes <samuelmendespy@gmail.com>
2025-03-02 07:42:39 -0300

    refactor: update car api route

--- rent/src/app/api/cars/route.ts
+++ rent/src/app/api/cars/route.ts
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const cars: Car[] = [
-    { id: 1, name: "Hyundai Santa Fe", minimun_salary: 250 },
-    { id: 2, name: "JEEP Grand Cheroke", minimun_salary: 250 },
-    { id: 3, name: "Ford Escape", minimun_salary: 220 },
-    { id: 4, name: "Toyota RAV4", minimun_salary: 240 },
-    { id: 5, name: "Kia Rio", minimun_salary: 300 },
-  ];
+const cars: Car[] = [
+  { id: 1, plate: "Hyundai Santa Fe", minimun_salary: 250 },
+  { id: 2, plate: "JEEP Grand Cheroke", minimun_salary: 250 },
+  { id: 3, plate: "Ford Escape", minimun_salary: 220 },
+  { id: 4, plate: "Toyota RAV4", minimun_salary: 240 },
+  { id: 5, plate: "Kia Rio", minimun_salary: 300 },
+];
 
-  return NextResponse.json(cars);
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const plate = searchParams.get("plate")?.toLowerCase() || "";
+
+  const filteredCars = cars.filter((car) =>
+    car.plate.toLowerCase().includes(plate)
+  );
+
+  return NextResponse.json({ cars: filteredCars });
 }
